5763eb0d56dac7ae73dfdbd381ec8dfdda916f99
macskeptic <macskeptic@gmail.com>
2010-06-19 13:39:18 -0300

    improvment

--- js/payment-methods.js
+++ js/payment-methods.js
@@ -1,10 +1,11 @@
 "use strict";
 
 $(function () {
-  $("input[name='payment_method']").change(function () {
+  $("input[name='payment_method']").bind('change', function () {
     var that = $(this);
     (that.val() === 'credit_card') ?
       $("fieldset#fields_for_credit_card").removeClass("hidden") :
       $("fieldset#fields_for_credit_card").addClass("hidden");
   });
+  $("#radio_credit_card").attr("checked", "checked").trigger('change');
 });
