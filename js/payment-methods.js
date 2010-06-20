38db2e75380eecf42ed545a5a94755d069cec2ed
macskeptic <macskeptic@gmail.com>
2010-06-19 22:33:55 -0300

    ok, looking good, gonna try moving the extra fieldset

--- js/payment-methods.js
+++ js/payment-methods.js
@@ -4,8 +4,8 @@ $(function () {
   $("input[name='payment_method']").bind('change', function () {
     var that = $(this);
     (that.val() === 'credit_card') ?
-      $("fieldset#fields_for_credit_card").removeClass("hidden") :
-      $("fieldset#fields_for_credit_card").addClass("hidden");
+      $("fieldset#fields_for_credit_card").slideDown() :
+      $("fieldset#fields_for_credit_card").slideUp();
   });
   $("#radio_credit_card").attr("checked", "checked").trigger('change');
 });
