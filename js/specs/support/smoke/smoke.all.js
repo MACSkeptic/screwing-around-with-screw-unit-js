ca4ada2cea9a7bd8298ade128dacc521f54ceddd
macskeptic <macskeptic@gmail.com>
2010-06-21 23:25:23 -0300

    added a smoke plugin to make it easier to use on screw unit

--- js/specs/support/smoke/smoke.all.js
+++ js/specs/support/smoke/smoke.all.js
@@ -272,3 +272,29 @@ Smoke.Stub.prototype = {
 		return this.obj;
 	}
 };
+
+// This is a lightweight bridge between Smoke and Screw.Unit.
+// It shadows mocking and stubbing onto the matchers to make them available within tests.
+
+Screw.Matchers.mock = function(m) {
+  return Smoke.Mock(m);
+};
+
+Screw.Matchers.mock_function = function(func,name) {
+  return Smoke.MockFunction(func,name);
+};
+
+Screw.Matchers.stub = function(obj, attr) {
+  return new Smoke.Stub(obj,attr);
+};
+
+(function($) {
+  $(Screw).bind("before.screwunit", function(){
+    function checkAndResetSmoke() {
+      Smoke.checkExpectations();
+      Smoke.reset();
+    }
+
+    $('.it').bind('finished_run.screwunit', function(){ checkAndResetSmoke(); });
+  });
+})(jQuery);
