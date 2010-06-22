c3088d854d7a6f4b7fc866e2dd47705498744183
macskeptic <macskeptic@gmail.com>
2010-06-22 00:24:04 -0300

    added some specs for the error api

--- js/specs/payment-methods/errors_spec.js
+++ js/specs/payment-methods/errors_spec.js
@@ -41,5 +41,37 @@ Screw.Unit(function () {
         });
       });
     });
+
+    describe('#[has|have]Occurred[On]', function () {
+      describe('when there is an error on one element', function () {
+        before(function () {
+          macskeptic.errors.add(elementId, 'i feel so depressed');
+        });
+
+        it('should return true for the given element', function () {
+          expect(macskeptic.errors.hasOccurred(elementId)).to(be_true);
+        });
+
+        it('should return true for other elements', function () {
+          expect(macskeptic.errors.hasOccurredOn(elementId + 'not')).to(be_false);
+          expect(macskeptic.errors.haveOccurredOn(elementId + 'on')).to(be_false);
+          expect(macskeptic.errors.haveOccurred(elementId + 'me')).to(be_false);
+        });
+
+        it('should return true if no element is given', function () {
+          expect(macskeptic.errors.hasOccurred()).to(be_true);
+        });
+      });
+      describe('when there are no errors', function () {
+        it('should return false if no element is given', function () {
+          expect(macskeptic.errors.haveOccurred()).to(be_false);
+        });
+
+        it('should return true if any element is given', function () {
+          expect(macskeptic.errors.haveOccurredOn(elementId)).to(be_false);
+          expect(macskeptic.errors.haveOccurredOn('foo' + elementId)).to(be_false);
+        });
+      });
+    });
   });
 });
