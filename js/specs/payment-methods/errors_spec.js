d6b50ef793d48d27ba28ed7579a9ab6635fbd2ff
macskeptic <macskeptic@gmail.com>
2010-06-22 00:29:32 -0300

    finished specs for the errors api, add and clean/clear will be tested indirectly

--- js/specs/payment-methods/errors_spec.js
+++ js/specs/payment-methods/errors_spec.js
@@ -62,6 +62,7 @@ Screw.Unit(function () {
           expect(macskeptic.errors.hasOccurred()).to(be_true);
         });
       });
+
       describe('when there are no errors', function () {
         it('should return false if no element is given', function () {
           expect(macskeptic.errors.haveOccurred()).to(be_false);
@@ -70,6 +71,45 @@ Screw.Unit(function () {
         it('should return true if any element is given', function () {
           expect(macskeptic.errors.haveOccurredOn(elementId)).to(be_false);
           expect(macskeptic.errors.haveOccurredOn('foo' + elementId)).to(be_false);
+        });
+      });
+    });
+    describe('#count', function () {
+      describe('when there is an error on one element', function () {
+        before(function () {
+          macskeptic.errors.add(elementId, 'i feel so depressed');
+        });
+
+        it('should return 1', function () {
+          expect(macskeptic.errors.count()).to(equal, 1);
+        });
+      });
+
+      describe('when there are errors on multiple elements', function () {
+        before(function () {
+          macskeptic.errors.add(elementId, 'i feel so depressed');
+          macskeptic.errors.add(elementId + 'other', 'i feel so depressed too');
+        });
+
+        it('should return the error count', function () {
+          expect(macskeptic.errors.count()).to(equal, 2);
+        });
+      });
+
+      describe('when there are multiple errors on a single element', function () {
+        before(function () {
+          macskeptic.errors.add(elementId, 'i feel so depressed');
+          macskeptic.errors.add(elementId, 'i feel even more depressed');
+        });
+
+        it('should return the error count', function () {
+          expect(macskeptic.errors.count()).to(equal, 2);
+        });
+      });
+
+      describe('when there are no errors', function () {
+        it('should return 0', function () {
+          expect(macskeptic.errors.count()).to(equal, 0);
         });
       });
     });
